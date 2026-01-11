21b7c0aaa15b6508afb34a7ac575f046912bdeed
ikohei0001 <labellavita140@gmail.com>
2026-01-11 13:34:27 +0900

    Add RSpec test file for problem9

---
+++ spec/problem9_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../lib/problem9"
+
+RSpec.describe "#custom_sort" do
+  context "when given an array of words" do
+    it "correctly sorts an array" do
+      expect(custom_sort(["peach", "apple", "banana"])).to eq(["apple", "banana", "peach"])
+    end
+
+    it "places a capitalized word after its lowercase version" do
+      expect(custom_sort(["Apple", "apple"])).to eq(["apple", "Apple"])
+    end
+
+    it "places an uppercase word after lowercase and capitalized words" do
+      expect(custom_sort(["APPLE", "apple", "Apple"])).to eq(["apple", "Apple", "APPLE"])
+    end
+  end
+end
