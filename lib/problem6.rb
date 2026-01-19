9419762e918d3e390790f5a339561cbdd541b7b5
ikohei0001 <labellavita140@gmail.com>
2026-01-19 20:40:16 +0900

    Update problem6.rb with new correct answer

--- lib/problem6.rb
+++ lib/problem6.rb
@@ -2,7 +2,7 @@ def divisors_using_sqrt(x)
   result = []
 # Math.sqrt(x)だと浮動小数点演算のため、xが平方数だと誤差が発生する可能性がある
 # Integer.sqrt(x)だと確実に整数を返してくれる
-  (1..Math.sqrt(x).to_i).each do |i|
+  (1..Integer.sqrt(x).to_i).each do |i|
     if x % i == 0
       result << i                     # 小さい約数を追加
       result << x / i unless i == x / i  # 対応する大きい約数を追加（重複防止）
@@ -11,22 +11,6 @@ def divisors_using_sqrt(x)
   result.sort
 end
 
-def divisors(value)
-  result = []
-  i = 1
-  median = Integer.sqrt(value)
-  
-  while i <= median
-    quotient, remainder = value.divmod(i)
-    
-    if remainder == 0
-      result << i
-      result << quotient unless i == quotient
-    end
-    i += 1
-  end
-  result.sort
-end
 
 def divisors_on(x)
   array = []
